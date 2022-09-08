35bd9b57b53efda7c04a244a6ce008a8713ebf23
serickson67 <shea.erickson67@gmail.com>
2022-09-07 20:49:47 -0400

    added useContext and useEffect hooks

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
+import { useContext } from 'react';
 import FeedbackItem from './FeedbackItem';
-import PropTypes from 'prop-types';
+import FeedbackContext from '../context/FeedbackContext';
 
-function FeedbackList({ feedback, handleDelete }) {
+function FeedbackList() {
+  const { feedback } = useContext(FeedbackContext);
+
   if (!feedback || feedback.length === 0) {
     return <p>No feedback yet</p>;
   }
+
+  return (
+    <div className='feedback-list'>
+      {feedback.map((item) => (
+        <FeedbackItem key={item.id} item={item} />
+      ))}
+    </div>
+  );
 
   // return (
   //   <div className='feedback-list'>
@@ -28,24 +39,6 @@ function FeedbackList({ feedback, handleDelete }) {
   //     </AnimatePresence>
   //   </div>
   // );
-
-  return (
-    <div className='feedback-list'>
-      {feedback.map((item) => (
-        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
-      ))}
-    </div>
-  );
 }
-
-FeedbackList.propTypes = {
-  feedback: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      text: PropTypes.string.isRequired,
-      rating: PropTypes.number.isRequired,
-    })
-  ),
-};
 
 export default FeedbackList;
--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { useContext } from 'react';
+import FeedbackContext from '../context/FeedbackContext';
 
-function FeedbackStats({ feedback }) {
+function FeedbackStats() {
+  const { feedback } = useContext(FeedbackContext);
+
   // Calculate ratings average
   let average =
     feedback.reduce((accumulator, currentValue) => {
@@ -17,9 +20,5 @@ function FeedbackStats({ feedback }) {
     </div>
   );
 }
-
-FeedbackStats.propTypes = {
-  feedback: PropTypes.array.isRequired,
-};
 
 export default FeedbackStats;
